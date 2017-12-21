b3c1d8158c098acd9ef1dbc6ece18a03891879b5
Mazens Zibara <mazen.zibara@makit.lv>
2017-12-21 23:58:42 +0200

    Add ajax fetch

--- static/index.js
+++ static/index.js
@@ -1,37 +1,12 @@
 $(document).ready(() => {
-
   $('#searchEvents').click(() => {
     const search = $('#name').val()
     const date = $('#date').val()
-    let url = `http://localhost:8090/events/search?search=${name}&date=${date}`
-
-    console.log(url)
-    console.log("Button clicked")
-          }
-        }
     $.ajax({
-      url,
+      url: `/events/search?search=${search}&date=${date}`, 
       success(data) {
-        $('#searchEvents').html("");
-        var events = JSON.parse(data).events;
-        for(i = 0; i < events.length; i++){
-        $('#col-sm-4').append("<div>\
-                          <strong>"+  search + "</strong></br>\
-                  </div>")
+        console.log(data)
       }
     })
-  }
-
-    // // $.getJSON("venues",function(data){
-    // //   console.log(data);
-    //   var test = data.test;
-    //   var header = $("<h2>").text(test);
-    //   $("body").append(header);
-    // // }
-    // //
-    // //
-    // //
-
-
   })
 })
