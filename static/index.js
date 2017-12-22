b68086eb5ad5204a08fc6391aa368a69c14fb720
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-12-22 17:26:47 +0300

    try the js loop

--- static/index.js
+++ static/index.js
@@ -3,10 +3,17 @@ $(document).ready(() => {
     const search = $('#name').val()
     const date = $('#date').val()
     $.ajax({
-      url: `/events/search?search=${search}&date=${date}`, 
+      url: `/events/search?search=${search}&date=${date}`,
       success(data) {
         console.log(data)
-      }
+         var events = data
+         for (i=0; i<events.length;i++){
+           // Shows underfined :(
+           // console.log(events.title);
+
+            $('#col-sm-4').append( document.createTextNode( "Hello" ) )
+         }
+       }
+     })
+      })
     })
-  })
-})
