d0ecdbf87c354f39bbd3f320863f74bd79a6ccb1
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-12-22 20:09:51 +0300

    fixed js :)

--- static/index.js
+++ static/index.js
@@ -7,13 +7,32 @@ $(document).ready(() => {
       success(data) {
         console.log(data)
          var events = data
+         var counter = 0
          for (i=0; i<events.length;i++){
-           var template = ['<div class="col-sm-4">\
-            <p> Event 1 </p>\
-            <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">\
-            </div>')].join("\n")
-            $('#row').append(template)
-         }
+
+           // var template = ['<div class="col-sm-4">\
+           //  <p> Event 1 </p>\
+           //  <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">\
+           //  </div>')]
+           counter +=1
+
+        //    "event_id": 1,
+        // "title": "Test event",
+        // "venue_id": 1,
+        // "date": "2025-11-19T21:00:00.000Z",
+        // "url": "",
+        // "blurb": ""
+           $('#eventRow').append('<div class="col-sm-4">\
+            <p> Event ' + counter + '</p>\
+            <p> Venue_id' + events[i].venue_id + '</p>\
+            <p> Date ' + events[i].date + '</p>\
+            <p> url:  ' + events[i].url + '</p>\
+            <p> blurb:  ' + events[i].blurb + '</p>\
+            </div>')
+          }
+
+
+
        }
      })
       })
