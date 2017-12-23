6f457d533c371ad62e76911edee1099cb1957048
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-12-23 14:10:04 +0300

    database added

--- static/index.js
+++ static/index.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  // This works perfect
     $('#searchEvents').click(() => {
       const search = $('#name').val()
       const date = $('#date').val()
@@ -20,12 +21,12 @@ $(document).ready(() => {
             }
         })
 
+        // This shows not authenticated
         $('#btnLogin').click(() => {
-          console.log("Here");
             const username = $('#username').val()
             const date = $('#password').val()
               $.ajax({
-                url: `/login/login?login=${username}&date=${password}`,
+                url: `/login/login?username=${username}&date=${password}`,
                 success(data) {
                 console.log(data)
                 var login = data
@@ -33,10 +34,49 @@ $(document).ready(() => {
                 $('#username').remove()
                 $('#password').remove()
                 $('#signInTxt').append("<div> SUCCESS </div>")
+                // Redirect
                 window.location.href = "/events/static/admin.html"
 
                 }
               })
           })
+
+          $('#btnSubminEvents').click(() => {
+            console.log("Here");
+              const username = $('#title').val()
+              const event_id = $('#event_id').val()
+              const venue_id = $('#venue_id').val()
+              const date = $('#date').val()
+              const blurb = $('#blurb').val()
+              const url = $('#url').val()
+                $.ajax({
+                  url: `/events/add?title=${title}&event_id=${event_id}&venue_id=${venue_id}&date=${date}&blurb=${blurb}&url=${url}`,
+                  success(data) {
+                  console.log(data)
+                  var login = data
+                  $('#btnSubminEvents').append('<div> Your event' + title + 'Have been added </div>')
+                  window.location.href = "/events/static/admin.html"
+                  }
+                })
+            })
+
+            $('#btnSubminVenues').click(() => {
+              console.log("Here");
+                const name = $('#venueName').val()
+                const postcode = $('#postcode').val()
+                const town = $('#town').val()
+                const url = $('#url').val()
+                const icon = $('#icon').val()
+                  $.ajax({
+                    url: `/venues/add?name=${name}&postcode=${postcode}&town=${town}&url=${url}&icon=${icon}`,
+                    success(data) {
+                    console.log(data)
+                    var login = data
+                    $('#btnSubminEvents').append('<div> Your venue' + name + 'Have been added </div>')
+                    window.location.href = "/events/static/admin.html"
+
+                    }
+                  })
+              })
    })
  })
