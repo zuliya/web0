da7a23931e7feffe74469419995c0d1f6b32e746
Zuzukina <Zuzukina@Zulikhats-MacBook-Pro.local>
2017-12-23 11:52:05 +0300

    admin added

--- static/index.js
+++ static/index.js
@@ -1,51 +1,42 @@
 $(document).ready(() => {
-  $('#searchEvents').click(() => {
-    const search = $('#name').val()
-    const date = $('#date').val()
-    $.ajax({
-      url: `/events/search?search=${search}&date=${date}`,
-      success(data) {
-        console.log(data)
-         var events = data
-         var counter = 0
-         for (i=0; i<events.length;i++){
+    $('#searchEvents').click(() => {
+      const search = $('#name').val()
+      const date = $('#date').val()
+      $.ajax({
+          url: `/events/search?search=${search}&date=${date}`,
+          success(data) {
+            console.log(data)
+             var events = data
+             var counter = 0
+               for (i=0; i<events.length;i++){
+                 counter +=1
+                 $('#eventRow').append('<div class="col-sm-4">\
+                  <p> <br> <span style="font-weight:bold"> Event ' + counter + '</span></p>\
+                  <p> Title: <br>' + events[i].title + '</p>\
+                  <p> Date: <br>' + events[i].date + '</p>\
+                  <p> url: <br> ' + events[i].url + '</p>\
+                  <br> \
+                  </div>')}
+            }
+        })
 
-           counter +=1
-
-           $('#eventRow').append('<div class="col-sm-4">\
-            <p> <br> <span style="font-weight:bold"> Event ' + counter + '</span></p>\
-            <p> Title: <br>' + events[i].title + '</p>\
-            <p> Date: <br>' + events[i].date + '</p>\
-            <p> url: <br> ' + events[i].url + '</p>\
-            <br> \
-            </div>')
-          }
-
-          $('#btnLogin').click(() => {
-            const search = $('#username').val()
+        $('#btnLogin').click(() => {
+          console.log("Here");
+            const username = $('#username').val()
             const date = $('#password').val()
-            $.ajax({
-              url: `/events/search?search=${search}&date=${date}`,
-              success(data) {
+              $.ajax({
+                url: `/login/login?login=${username}&date=${password}`,
+                success(data) {
                 console.log(data)
-                 var events = data
-                 var counter = 0
-                 for (i=0; i<events.length;i++){
+                var login = data
+                $('#btnLogin').remove()
+                $('#username').remove()
+                $('#password').remove()
+                $('#signInTxt').append("<div> SUCCESS </div>")
+                window.location.href = "/events/static/admin.html"
 
-                   counter +=1
-
-                   $('#eventRow').append('<div class="col-sm-4">\
-                    <p> <br> <span style="font-weight:bold"> Event ' + counter + '</span></p>\
-                    <p> Title: <br>' + events[i].title + '</p>\
-                    <p> Date: <br>' + events[i].date + '</p>\
-                    <p> url: <br> ' + events[i].url + '</p>\
-                    <br> \
-                    </div>')
-                  }
-
-
-
-       }
-     })
-      })
-    })
+                }
+              })
+          })
+   })
+ })
